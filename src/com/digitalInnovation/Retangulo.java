ebc064667f19b85ec82ad67f7fba515bfff87199
Gilvan Alves <gilvan.dev@gmail.com>
2022-03-18 18:04:43 -0300

    Criação da classe retângulo

---
+++ src/com/digitalInnovation/Retangulo.java
@@ -0,0 +1,48 @@
+package com.digitalInnovation;
+/*
+Crie uma classe que modele um retangulo:
+Atributos: LadoA, LadoB (ou Comprimento e Largura, ou Base e Altura, a escolher).
+Métodos: Mudar valor dos lados, Retornar valor dos lados, calcular Área e calcular Perímetro.
+ */
+public class Retangulo {
+
+    public Double base;
+    public Double altura;
+
+    public Retangulo(Double base, Double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public void alterarValor(){
+
+    }
+
+    public void retornarValor(){
+
+    }
+
+    public void calcularArea(){
+
+    }
+
+    public void calcularPerimetro(){
+
+    }
+
+    public Double getBase() {
+        return base;
+    }
+
+    public void setBase(Double base) {
+        this.base = base;
+    }
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Double altura) {
+        this.altura = altura;
+    }
+}
